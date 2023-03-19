c82a657962742171ab9e8a0ea10f7734828498a6
miku-lin <126040842+miku-lxc@users.noreply.github.com>
2023-03-19 16:57:12 +0800

    Add files via upload

---
+++ mysql/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /miku/docker/docker_all/DOCEKR/mysql   ##这是我当前的文件夹，之后会上传到云服务的对应位置 /miku/docker/docker_all/下 
+
+###github 上传和下载
+sudo docker build -t miku-mysql:5.7 .
+
+
+
---
+++ php/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /miku/docker/docker_all/DOCEKR/php   ##这是我当前的文件夹，之后会上传到云服务的对应位置 /miku/docker/docker_all/下 
+
+###github 上传和下载
+sudo docker build -t miku-php:7.4 .
+
+
+
